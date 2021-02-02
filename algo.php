4fb92de2443bcc020a2234e242226ce35530371f
Upsas <deivydas.brusokas@gmail.com>
2021-02-02 15:09:27 +0200

    nauja versija

--- algo.php
+++ algo.php
@@ -5,7 +5,6 @@ $string = strtolower('Mistranslate');
 
 $newString = implode(" ", str_split($string, 1));
 $newString = str_split($newString);
-print_r($newString);
 foreach ($fa as $value) {
 
     $haystack = preg_replace('/[0-9]+/', '', $value);
@@ -14,13 +13,32 @@ foreach ($fa as $value) {
     $value = trim($value);
 
     if (strpos($value, '.') === 0 && str_starts_with($string, $haystack)) {
-        echo strpos($string, $haystack);
+        $value = trim($value, '.');
+        $pos = strpos($string, $haystack) * 2;
+        $value = implode(" ", str_split($value, 1));
+        $arr[$pos] = str_split($value);
+        $s[$pos] = (array_filter(str_split($value), 'is_numeric'));
+        print_r($s);
+    }if (strpos($string, $haystack) !== false && strpos($value, '.') !== 0) {
 
-    } else if (strpos($string, $haystack) !== false && strpos($value, '.') !== 0) {
-        echo strpos($string, $haystack);
+        $value = trim($value, '.');
+        $pos = strpos($string, $haystack) * 2;
+        $value = implode(" ", str_split($value, 1));
+        $arr[$pos] = str_split($value);
+        $asf = array_filter(str_split($value), 'is_numeric');
+        $m[$pos][] = (array_filter(str_split($value), 'is_numeric'));
 
-        $abc[] = $value;
-    }if (strpos($value, '.', 1) && str_ends_with($string, substr($haystack, 0, -1))) {
+    }if (strpos($value, '.') > 0 && str_ends_with($string, substr($haystack, 0, -1))) {
+
+        $haystack = substr($haystack, 0, -1);
+        $pos = strpos($string, $haystack) * 2;
+        $value = implode(" ", str_split($value, 1));
+        $arr[$pos] = str_split($value);
+        $e[$pos] = (array_filter(str_split($value), 'is_numeric'));
 
     }
 }
+
+// print_r($s);
+print_r($m);
+// print_r($e);
