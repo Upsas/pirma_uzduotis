e2c8fcf23453390b3ebaa0424f88c820072ac6be
Upsas <deivydas.brusokas@gmail.com>
2021-02-03 10:59:41 +0200

    v2

--- algo.php
+++ algo.php
@@ -27,7 +27,6 @@ foreach ($fa as $value) {
 }
 
 $allValues = array_values(array_unique($allValues));
-
 function stripNumbers($allValues)
 {
     //grazina patterna be skaiciu (ir tasku gal)
@@ -38,57 +37,75 @@ function stripNumbers($allValues)
 function getPositionOfPattern($string, $allValues)
 {
     $strippedPattern = stripNumbers($allValues);
-    print_r($strippedPattern); //nuema skaicius nuo patterno, jeigu yra taskai tai checkina kad butu pradzia/pabaiga zodzio
+    //nuema skaicius nuo patterno, jeigu yra taskai tai checkina kad butu pradzia/pabaiga zodzio
     //jeigu randa patterna tai grazina int su pozicija, jeigu ne -1 arba exceptiona
+
     if (strpos($strippedPattern, '.') === 0) {
-        return strpos($string, $strippedPattern);
-    } else if ((strpos($strippedPattern, -1) === '.')) {
-        return strpos($string, $strippedPattern);
+        return intval(strpos($string, trim($strippedPattern . ' ')));
+    } else if ((substr($strippedPattern, -1) === '.')) {
+        return intval(strpos($string, trim($strippedPattern, '.')));
     } else {
-        return strpos($string, $strippedPattern);
+        return intval(strpos($string, $strippedPattern));
     }
     if (!strpos($string, $strippedPattern)) {
-        return false;
+        return -1;
     }
 
 };
 
+function populateNumbersArray($numbersArray, $position, $allValues)
+{
+
+    //iteruoji per kiekviena patterno chara,
+
+    for ($i = 0; $i < strlen($allValues); $i++) {
+        //jeigu skaiciumi prasideda tai $numbersArray[$position] = $tasSkaicius
+        if (is_numeric($allValues[0])) {
+            $numbersArray[$position] = $allValues[0];
+            break;
+        }
+        // //jeigu ne tai position+1
+
+        else { $position = $position + 1;}
+
+        //ir checkini kai yra skaiciu imeti ji i $numbersArray[$position] (tik pries mesdamas patikrini ar didesnis skaicius negu dabar yra, jeigu mazesnis tai nelieti ir judi toliau)
+
+        if (is_numeric($allValues[$i]) && !is_numeric($allValues[0])) {
+
+            if ($numbersArray[$position] < $allValues[$i]) {
+                $numbersArray[$position] = $allValues[$i];
+                break;
+            }
+
+        }
+    }
+    //pabaigoje grazini ta array su skaiciais
+    return ($numbersArray);
+
+}
 foreach ($allValues as $pattern) {
     $position = getPositionOfPattern($string, $pattern);
     if ($position > -1) {
         $numbersArray = populateNumbersArray($numbersArray, $position, $pattern);
     }
 }
-
-$result = getResult();
-function populateNumbersArray($numbersArray, $position, $allValues)
-{
-
-    //iteruoji per kiekviena patterno chara,
-    foreach ($allValues as $patter) {
-        //jeigu skaiciumi prasideda tai $numbersArray[$position] = $tasSkaicius
-        if (is_numeric($patter[0])) {
-            $numbersArray[$position] = $patter[0];
-        } else { $position = $position + 1;} //jeigu ne tai position+1
-        //ir checkini kai yra skaiciu imeti ji i $numbersArray[$position] (tik pries mesdamas patikrini ar didesnis skaicius negu dabar yra, jeigu mazesnis tai nelieti ir judi toliau)
-        for ($i = 0; $i < strlen($patter); $i++) {
-            if (is_numeric($patter[$i])) {
-                // echo ' pos: ' . $position . ' ';
-            }
-        }
-    }
-
-    return $numbersArray;
-    //pabaigoje grazini ta array su skaiciais
-}
-populateNumbersArray($numbersArray, $position, $allValues);
-// print_r(populateNumbersArray($numbersArray, $position, $allValues));
 function mergeNumbersWithWord($numbersArray, $string)
 {
-    // return array_merge($numbersArray, str_split($string));
+    $newString = str_split($string);
+    for ($i = 0; $i < strlen($string); $i++) {
+        if (is_numeric($numbersArray[$i])) {
+            $newString[$i * 2] = $numbersArray[$i];
+
+        }
+    }
+    return $newString;
+
 }
-// echo mergeNumbersWithWord($numbersArray, $string);
-function getResult()
+
+print_r(mergeNumbersWithWord($numbersArray, $string));
+
+function getResult($numbersArray, $string)
 {
-    //callina mergeNumbers ir pakeicia skaicius i - jeigu reikia, jeigu ne tai istrina skaiciu
+
+    return implode(' ', mergeNumbersWithWord($numbersArray, $string));
 }
