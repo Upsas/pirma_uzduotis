1c3db29e17f730990c70e13a2ce18d5701396499
Deivydas Brusokas <61560970+Upsas@users.noreply.github.com>
2021-02-19 15:51:46 +0200

    Delete pirma_uzduotis directory

--- pirma_uzduotis/output.php
+++
@@ -1,7 +0,0 @@
-<?php
-include './algo.php';
-echo getResult($numbersArray, $string);
-
-$end_time = microtime(true);
-echo " time: ", bcsub($end_time, $start_time, 4) . PHP_EOL;
-echo " memory (byte): ", memory_get_peak_usage(true) . PHP_EOL;
